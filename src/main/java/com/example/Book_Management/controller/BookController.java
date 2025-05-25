04ef0167131dec63564e7d1637d78ed94e77659e
gdghanu-nguyentrinhvo <gdschanu.vont0101@gmail.com>
2025-05-25 20:10:59 +0700

    Add BookController class

---
+++ src/main/java/com/example/Book_Management/controller/BookController.java
@@ -0,0 +1,43 @@
+package com.example.Book_Management.controller;
+
+import com.example.Book_Management.dto.FilterParamBook;
+import com.example.Book_Management.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@AllArgsConstructor
+public class BookController {
+    @Autowired
+    private final BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<?> getBook(@RequestParam(required = false) Long id,
+                                     @RequestParam(required = false) Long idMode
+    ){
+        FilterParamBook filterParamBook = new FilterParamBook();
+        filterParamBook.setId(id);
+        filterParamBook.setIdMode(idMode);
+        return ResponseEntity.ok(bookService.getBook(filterParamBook));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createBook(@RequestBody Book book){
+        return ResponseEntity.ok(bookService.createBook(book));
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> updateBook(@RequestBody Book book){
+        return ResponseEntity.ok(bookService.updateBook(book));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable("id") long id){
+        return ResponseEntity.ok(bookService.deleteBook(id));
+    }
+}
