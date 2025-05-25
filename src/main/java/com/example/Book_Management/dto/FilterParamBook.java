2bc636b4c6a9a7a6437d4c5e055b2fab55d61f0a
gdghanu-nguyentrinhvo <gdschanu.vont0101@gmail.com>
2025-05-25 20:11:23 +0700

    Add FilterParamBook class

---
+++ src/main/java/com/example/Book_Management/dto/FilterParamBook.java
@@ -0,0 +1,17 @@
+package com.example.Book_Management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FilterParamBook {
+    private Long idMode;
+    private Long id;
+}
